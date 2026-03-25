b151b722e768de239446fe3dcbd593fdbeaa5e07
francisigoe <frankisigoe@gmail.com>
2026-03-25 18:16:32 +0000

    fixed asteroid indetation and turning

--- asteroid.py
+++ asteroid.py
@@ -5,9 +5,9 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
-        def draw(self,screen):
-            pygame.draw.circle(screen,"white",self.position,self.radius,LINE_WIDTH)
+    def draw(self,screen):
+        pygame.draw.circle(screen,"white",self.position,self.radius,LINE_WIDTH)
 
-        def update(self, dt):
-            self.position += (self.velocity * dt)
+    def update(self, dt):
+        self.position += (self.velocity * dt)
         
