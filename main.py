d03cc35b26a54cd1d0589eba790b966bae37a246
francisigoe <frankisigoe@gmail.com>
2026-03-25 17:43:20 +0000

    upto lesson 4-1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pygame 
+import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
 from constants import *
 from logger import log_state
 
@@ -10,15 +13,36 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field_1 = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field_1 = AsteroidField()
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass    
-        screen.fill("black")
-        pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return    
+        
+        updatable.update(dt)
+        screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
+        
+        pygame.display.flip()
+        dt = (time.tick(60)) / 1000
+
+            
+
 
 if __name__ == "__main__":
     main()
